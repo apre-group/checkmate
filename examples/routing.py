be3eb362090564b373f44b19f085d48b66b426e7
Anja Petković Komel <anja.petkovic@gmail.com>
2022-11-15 11:29:05 +0100

    WIP: routing: sharing with a subset of players

--- examples/routing.py
+++ examples/routing.py
@@ -2,6 +2,7 @@ from os import stat
 from dsl import *
 from typing import Union
 import copy
+import itertools
 
 A, E1, I, E2, B = players('A', 'E1', 'I', 'E2', 'B')
 S_H, L, U, J, S_S, L_T, L_H, L_A, S_SE1, S_SI, I_L, I_U, I_S, S_SA, S_SE2, S_SB = actions(
@@ -34,10 +35,28 @@ recursion_depth = 0
 
 ps = [A, E1, I, E2, B]
 
-for player in ps:
-    for p in ps:
-        # sharing player's secret with p
-        ACTIONS.append(Action(f"S_S{p}_{player}"))
+
+# Actions for sharing secrets: S_S_[[],[],[A, B], [], [E1]]
+powerset = [subset
+            for length in range(len(ps)+1)
+            for subset in itertools.combinations(ps, length)]
+for ll in itertools.product(powerset, repeat=5):
+    ACTIONS.append(Action(f"S_S_{ll}"))
+
+
+def secret_sharing_sets(secrets_to_share: Dict[Player, List[Player]]):
+    powersets = []
+    for key in secrets_to_share:
+        powerset = [subset
+                    for length in range(len(secrets_to_share[key]) + 1)
+                    for subset in itertools.combinations(secrets_to_share[key], length)]
+        powersets.append(powerset)
+    for ll in itertools.product(*powersets):
+        yield ll
+
+
+# for x in secret_sharing_sets({A: [], E1: [A], I: [], E2: [], B: [E1, E2]}):
+#     print(x)
 
 
 def prev_player(player):
@@ -142,11 +161,8 @@ def next_player(state):
     return next_p, state2
 
 
-def share_secret_action(player, secret):
-    # Delete me
-    #if secret != A and secret != B:
-    #    raise Exception("We are not using the representative for the secret!")
-    return Action(f"S_S{player}_{secret}")
+def share_secret_action(sharing_instance):
+    return Action(f"S_S{sharing_instance}")
 
 
 def align_secret_knowledge(state):
@@ -162,7 +178,7 @@ def align_secret_knowledge(state):
 
 
 def generate_routing_unlocking(player: Player, state, history):
-    # print(history)
+    print(history)
     # print(player)
     # print(state)
     # we can assume that current player p knows the secret.
@@ -184,16 +200,25 @@ def generate_routing_unlocking(player: Player, state, history):
         for eq_class in state["eq_secrets"]:
             if eq_class[0] in secrets_to_share:
                 secrets_to_share_representatives.append(eq_class[0])
-        # sharing_dict = {s: [] for s in secrets_to_share}
-        for secret in secrets_to_share_representatives:
-            for p in set(ps) - {player}:
-                if not state[p]["secrets"][secret]:
-                    # sharing_dict[secret].append(p)
-                    state1 = copy_state(state)
-                    state1[p]["secrets"][secret] = True
-                    align_secret_knowledge(state1)
-                    next_p, state2 = next_player(state1)
-                    branch_actions[share_secret_action(p, secret)] = generate_routing_unlocking(next_p, state2, history + str(player) + "." + str(share_secret_action(p, secret)) + ";")
+        if history:
+            pprev_player, prev_action = history[:-1].split(";")[-1].split(".")
+        else:
+            pprev_player = None
+            prev_action = None
+        if pprev_player != str(player) or "S_S" not in prev_action:
+            sharing_dict = {s: [] for s in ps}
+            for secret in secrets_to_share_representatives:
+                for p in set(ps) - {player}:
+                    if not state[p]["secrets"][secret]:
+                        sharing_dict[secret].append(p)
+            for sharing_instance in secret_sharing_sets(sharing_dict):
+                state1 = copy_state(state)
+                for i in range(5):
+                    for person in sharing_instance[i]:
+                        state1[person]["secrets"][ps[i]] = True
+                align_secret_knowledge(state1)
+                next_p, state2 = next_player(state1)
+                branch_actions[share_secret_action(sharing_instance)] = generate_routing_unlocking(next_p, state2, history + str(player) + "." + str(share_secret_action(sharing_instance)) + ";")
         if state[player]["contract"] == "locked" and state[player]["secrets"][prev_player(player)]:
             # Action unlock
             state1 = copy_state(state)
@@ -270,6 +295,7 @@ intermediate_state = {
 unlocking_tree = generate_routing_unlocking(B, intermediate_state, "")
 # my_tree = generate_routing_unlocking(E2, intermediate_state)
 
-tree(unlocking_tree)
+# tree(unlocking_tree)
 
-finish()
+# finish()
+
