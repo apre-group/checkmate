dfa1d61fe055f3fbf10d94fd86053f98cbb2099c
Michael Rawson <michael@rawsons.uk>
2022-11-28 13:03:50 +0000

    DSL simplifies expressions

--- examples/dsl.py
+++ examples/dsl.py
@@ -28,129 +28,200 @@ class Player(StringThing):
     pass
 
 
-class Precedence(IntEnum):
-    ADDITION = 0,
-    SUBTRACTION = 1,
-    MULTIPLICATION = 2,
-    HIGHEST = 3
-
-
 class Expr:
-    precedence: Precedence
+    def __add__(self, other: LExpr) -> LExpr:
+        return add_expr(self, other)
 
-    def __init__(self, precedence: Precedence):
-        self.precedence = precedence
+    def __radd__(self, other: LExpr) -> LExpr:
+        return add_expr(other, self)
 
-    def __add__(self, other: Union[Expr, float, int]) -> Expr:
-        return binary_expr(self, other, '+')
+    def __sub__(self, other: LExpr) -> LExpr:
+        return sub_expr(self, other)
 
-    def __radd__(self, other: Union[Expr, float, int]) -> Expr:
-        return binary_expr(other, self, '+')
+    def __rsub__(self, other: LExpr) -> LExpr:
+        return sub_expr(other, self)
 
-    def __sub__(self, other: Union[Expr, float, int]) -> Expr:
-        return binary_expr(self, other, '-')
+    def __mul__(self, other: LExpr) -> LExpr:
+        return mul_expr(self, other)
 
-    def __rsub__(self, other: Union[Expr, float, int]) -> Expr:
-        return binary_expr(other, self, '-')
+    def __rmul__(self, other: LExpr) -> LExpr:
+        return mul_expr(other, self)
 
-    def __mul__(self, other: Union[Expr, float, int]) -> Expr:
-        return binary_expr(self, other, '*')
+    def __neg__(self) -> TermExpr:
+        return neg_expr(self)
 
-    def __rmul__(self, other: Union[Expr, float, int]) -> Expr:
-        return binary_expr(other, self, '*')
-
-    def __neg__(self) -> Expr:
-        return negate_expr(self)
-
-    def __ne__(self, other: Union[Expr, float, int]) -> Constraint:
+    def __ne__(self, other: LExpr) -> Constraint:
         return DisequationConstraint('!=', self, other)
 
-    def __gt__(self, other: Union[Expr, float, int]) -> Constraint:
+    def __gt__(self, other: LExpr) -> Constraint:
         return DisequationConstraint('>', self, other)
 
-    def __ge__(self, other: Union[Expr, float, int]) -> Constraint:
+    def __ge__(self, other: LExpr) -> Constraint:
         return DisequationConstraint('>=', self, other)
 
     def json(self):
         return repr(self)
 
 
-LExpr = Union[Expr, float, int]
-
-
-def precedence_of(expr: LExpr) -> Precedence:
-    return expr.precedence if isinstance(expr, Expr) else Precedence.HIGHEST
-
-
 class NameExpr(Expr):
     name: str
 
     def __init__(self, name: str):
-        super().__init__(Precedence.HIGHEST)
         self.name = name
 
     def __repr__(self):
         return self.name
 
 
-class ParenExpr(Expr):
-    expr: LExpr
+LExpr = Union[Expr, float, int]
 
-    def __init__(self, expr: LExpr):
-        super().__init__(Precedence.HIGHEST)
-        self.expr = expr
+
+def is_exactly(expr: LExpr, value: float) -> bool:
+    return (isinstance(expr, float) or isinstance(expr, int)) and expr == value
+
+
+class TermExpr(Expr):
+    constant: float
+    coefficients: Dict[Expr, float]
+
+    def __init__(self, constant: float, coefficients: Dict[Expr, float]):
+        self.constant = constant
+        self.coefficients = coefficients
+
+    @staticmethod
+    def from_expr(expr: LExpr) -> TermExpr:
+        if isinstance(expr, TermExpr):
+            return expr
+        elif isinstance(expr, float) or isinstance(expr, int):
+            return TermExpr(float(expr), {})
+
+        return TermExpr(0.0, {expr: 1})
+
+    @staticmethod
+    def negate(expr: TermExpr) -> TermExpr:
+        constant = -expr.constant
+        coefficients = {
+            key: -value
+            for key, value in expr.coefficients.items()
+        }
+        return TermExpr(constant, coefficients)
+
+    @staticmethod
+    def add(left: TermExpr, right: TermExpr) -> TermExpr:
+        constant = left.constant + right.constant
+        coefficients = dict(left.coefficients)
+        for key, value in right.coefficients.items():
+            if key in coefficients:
+                coefficients[key] += value
+                if coefficients[key] == 0.0:
+                    del coefficients[key]
+            else:
+                coefficients[key] = value
+
+        return TermExpr(constant, coefficients)
+
+    @staticmethod
+    def sub(left: TermExpr, right: TermExpr) -> TermExpr:
+        return TermExpr.add(left, TermExpr.negate(right))
+
+    @staticmethod
+    def mul_constant(expr: TermExpr, factor: float) -> TermExpr:
+        constant = factor * expr.constant
+        coefficients = {
+            key: factor * value
+            for key, value in expr.coefficients.items()
+        }
+        return TermExpr(constant, coefficients)
+
 
     def __repr__(self):
-        return f"({self.expr})"
+        if not self.coefficients:
+            return str(self.constant)
+
+        positive = {
+            key: value
+            for key, value in self.coefficients.items()
+            if value > 0.0
+        }
+        negative = {
+            key: abs(value)
+            for key, value in self.coefficients.items()
+            if value < 0.0
+        }
+
+        result = str(self.constant) if self.constant > 0.0 else ''
+        need_join = self.constant > 0.0
+
+        for key, value in positive.items():
+            factor = '' if value == 1.0 else f'{value} * '
+            join = ' + ' if need_join else '';
+            need_join = True
+            result += f'{join}{factor}{key}'
+
+        for key, value in negative.items():
+            factor = '' if value == 1.0 else f'{value} * '
+            join = ' - ' if need_join else '-';
+            need_join = True
+            result += f'{join}{factor}{key}'
+
+        if self.constant < 0.0:
+            result += f" - {abs(self.constant)}"
+
+        return result
+
+def neg_expr(expr: LExpr) -> TermExpr:
+    return TermExpr.negate(TermExpr.from_expr(expr))
 
 
-def parenthesise_expr(expr: LExpr, precedence: Precedence) -> Union[Expr, float, int]:
-    return expr if precedence <= precedence_of(expr) else ParenExpr(expr)
+def add_expr(left: LExpr, right: LExpr) -> LExpr:
+    if is_exactly(left, 0.0):
+        return right
+    elif is_exactly(right, 0.0):
+        return left
+
+    left = TermExpr.from_expr(left)
+    right = TermExpr.from_expr(right)
+    return TermExpr.add(left, right)
 
 
-class NegateExpr(Expr):
-    expr: LExpr
+def sub_expr(left: LExpr, right: LExpr) -> LExpr:
+    if is_exactly(right, 0.0):
+        return left
 
-    def __init__(self, expr: LExpr):
-        super().__init__(Precedence.HIGHEST)
-        self.expr = expr
-
-    def __repr__(self):
-        return f"-{self.expr}"
+    left = TermExpr.from_expr(left)
+    right = TermExpr.from_expr(right)
+    return TermExpr.sub(left, right)
 
 
-def negate_expr(expr: LExpr) -> Expr:
-    expr = parenthesise_expr(expr, Precedence.HIGHEST)
-    return NegateExpr(expr)
-
-
-BINARY_OP_PRECEDENCES = {
-    '+': (Precedence.ADDITION, Precedence.ADDITION, Precedence.ADDITION),
-    '-': (Precedence.ADDITION, Precedence.SUBTRACTION, Precedence.ADDITION),
-    '*': (Precedence.MULTIPLICATION, Precedence.MULTIPLICATION, Precedence.MULTIPLICATION)
-}
-
-
-class BinaryExpr(Expr):
-    op: str
+class MultiplicationExpr(Expr):
     left: LExpr
     right: LExpr
 
-    def __init__(self, op: str, precedence: Precedence, left: LExpr, right: LExpr):
-        super().__init__(precedence)
-        self.op = op
+    def __init__(self, left: LExpr, right: LExpr):
         self.left = left
         self.right = right
 
     def __repr__(self):
-        return f"{self.left} {self.op} {self.right}"
+        left = f"({self.left})" if isinstance(self.left, TermExpr) else f"{self.left}"
+        right = f"({self.right})" if isinstance(self.right, TermExpr) else f"{self.right}"
+        return f"{left} * {right}"
 
 
-def binary_expr(left: LExpr, right: LExpr, op: str) -> Expr:
-    lprec, rprec, precedence = BINARY_OP_PRECEDENCES[op]
-    left = parenthesise_expr(left, lprec)
-    right = parenthesise_expr(right, rprec)
-    return BinaryExpr(op, precedence, left, right)
+def mul_expr(left: LExpr, right: LExpr) -> LExpr:
+    if is_exactly(left, 0.0) or is_exactly(right, 0.0):
+        return 0.0
+    elif is_exactly(left, 1.0):
+        return right
+    elif is_exactly(right, 1.0):
+        return left
+    elif isinstance(left, int) or isinstance(left, float):
+        right = TermExpr.from_expr(right)
+        return TermExpr.mul_constant(right, float(left))
+    elif isinstance(right, int) or isinstance(right, float):
+        left = TermExpr.from_expr(left)
+        return TermExpr.mul_constant(left, float(right))
+
+    return MultiplicationExpr(left, right)
 
 
 class Tree:
